c23cf0cebe9fbd7d8954aa45ea4b924b213fd3f7
ozodbekabdullajonov12 <ozodbekabdullajonov1@gmail.com>
2024-09-24 22:35:31 +0500

    Uyga Vazifa4

---
+++ 4.py
@@ -0,0 +1,17 @@
+data = [
+    {"model": "RDX", "year": 2009},
+    {"model": "LS", "year": 2000},
+    {"model": "GLK-Class", "year": 2010},
+    {"model": "Express 1500", "year": 2005},
+    {"model": "LR2", "year": 2008},
+    {"model": "XF", "year": 2012},
+    {"model": "MR2", "year": 2005},
+    {"model": "Malibu", "year": 2007},
+    {"model": "M-Class", "year": 2010},
+    {"model": "Routan", "year": 2011}
+]
+
+sort = sorted(data, key = lambda x: x['year'])
+
+for x in sort:
+    print(f"Model: {x['model']}, Year: {x['year']}")
