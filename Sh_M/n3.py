8fbd4026ea80aa076248a593175b1ebec45c486e
ShukurulloPy <ismoilovasror047@gmail.com>
2024-09-24 22:06:56 +0500

    M_Sh

---
+++ Sh_M/n3.py
@@ -0,0 +1,17 @@
+ber = [
+    {"year": 2009},
+    {"model": "LS", "year": 2000},
+    {"model": "GLK-Class", "year": 2010},
+    {"model": "Express 1500", "year": 2005},
+    {"model": "LR2", "year": 2008},
+    {"model": "XF", "year": 2012},
+    {"model": "MR2", "year": 2005},
+    {"model": "Malibu", "year": 2007},
+    {"model": "M-Class", "year": 2010},
+    {"model": "Routan", "year": 2011}
+]
+
+rr = sorted(ber, key=lambda x: x["year"])
+
+for x in rr:
+    print(x)
